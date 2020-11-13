815223698deca099fa360de7db9d3f5fbbf9a82e
David Sterry <sterry.david@gmail.com>
2020-11-13 10:45:04 -0800

    refail merYears test

--- _tests_/planets.test.js
+++ _tests_/planets.test.js
@@ -13,26 +13,26 @@ describe('Person', () => {
   });
 
   test('should correctly return persons age on mercury.', () => {
-    expect(resuableHuman.mercYears()).toEqual(2)
+    expect(resuableHuman.mercYears()).toEqual(41)
   });
   
-  test('should correctly return persons age on venus.', () => {
-   expect(resuableHuman.venYears()).toEqual(6)
-  });
+ //test('should correctly return persons age on venus.', () => {
+   //expect(resuableHuman.venYears()).toEqual(6)
+  //});
 
-  test('should correctly return persons age on mars.', () => {
-    expect(resuableHuman.marsYears()).toEqual(18)
-  });
+  //test('should correctly return persons age on mars.', () => {
+   // expect(resuableHuman.marsYears()).toEqual(18)
+ // });
 
-  test('should correctly return person age on mars' , () => {
-    expect(resuableHuman.jupYears()).toEqual(118)
-  });
+  //test('should correctly return person age on mars' , () => {
+   // expect(resuableHuman.jupYears()).toEqual(118)
+  //});
 
-  test('should correctly return life expectancy' , () => {
-    expect (resuableHuman.lifeExpect()).toEqual(81)
-  });
+  //test('should correctly return life expectancy' , () => {
+   // expect (resuableHuman.lifeExpect()).toEqual(81)
+  //});
 
-  test('should correctly return users life expectancy on mercury.' , () => {
-    expect (resuableHuman.merLife()).toEqual()
-  })
+  //test('should correctly return users life expectancy on mercury.' , //() => {
+   // expect (resuableHuman.mercLife()).toEqual(78)
+ // })
 });

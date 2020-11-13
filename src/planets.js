6de75f80eaa520b73627cd1f442863af51e16dfd
David Sterry <sterry.david@gmail.com>
2020-11-13 10:45:37 -0800

    repass merYears test

--- src/planets.js
+++ src/planets.js
@@ -4,27 +4,30 @@ export class Person {
     this.gender = gender;
   }
 
-  lifeExpect() {
-    if (this.gender === "male"){
-      return 76;
-    } else if (this.gender === "female"){
-      return 81;
-    }
-  }
+  //lifeExpect() {
+    //if (this.gender === "male"){
+     // return 76;
+    //} else if (this.gender === "female"){
+     // return 81;
+    //}
+ // }
   mercYears() {
-    return Math.floor(this.age * .24);
+    return Math.floor(this.age /.24);
   }
 
-  venYears() {
-    return Math.floor(this.age * .62);
-  }
+ // venYears() {
+    //return Math.floor(this.age /.62);
+  //}
 
-  marsYears() {
-    return Math.floor(this.age * 1.88);
-  }
+  //marsYears() {
+    //return Math.floor(this.age / 1.88);
+  //}
 
-  jupYears() {
-    return Math.floor(this.age * 11.86);
-  }
+  //jupYears() {
+    //return Math.floor(this.age / 11.86);
+  //}
    
-}
+ /// mercLife() {
+    //return Math.floor((this.lifeExpect() - this.age) / .24);
+  }
+//}

39c360f3b8b21313c030ef9fe88518f9c8bd7575
David Sterry <sterry.david@gmail.com>
2020-11-13 09:37:37 -0800

    add test for merc years, purposefully fail

--- _tests_/planets.test.js
+++ _tests_/planets.test.js
@@ -1,10 +1,18 @@
 import { Person } from './../src/planets.js';
 
 describe('Person', () => {
+  let resuableHuman;
+
+  beforeEach(() => {
+    resuableHuman = new Person(10,"female")
+  })
   
   test('should correctly return persons age and gender.', () => {
-    const human = new Person(10, "male");
-    expect(human.age).toEqual(10);
-    expect(human.gender).toEqual("male")
+    expect(resuableHuman.age).toEqual(10);
+    expect(resuableHuman.gender).toEqual("female")
   });
+
+  test('should correctly return persons age on mercury.', () => {
+    expect(resuableHuman.mercyears).toEqual(1.2)
+  })
 });

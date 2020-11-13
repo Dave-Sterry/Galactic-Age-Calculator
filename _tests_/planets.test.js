c37599dfc68d63df72acccbe7a886d75ce9dce82
David Sterry <sterry.david@gmail.com>
2020-11-13 09:24:49 -0800

    purposefully pass test

--- _tests_/planets.test.js
+++ _tests_/planets.test.js
@@ -4,7 +4,7 @@ describe('Person', () => {
   
   test('should correctly return persons age and gender.', () => {
     const human = new Person(10, "male");
-    expect(human.age).toEqual(3);
-    expecd(human.gender).toEqual("male")
+    expect(human.age).toEqual(10);
+    expect(human.gender).toEqual("male")
   });
 });

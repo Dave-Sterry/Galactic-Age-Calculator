895d97f9b3d11ffa43e24c2573606df05e760fcb
David Sterry <sterry.david@gmail.com>
2020-11-19 12:14:06 -0800

    comment out non functioning code, check run build

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { Person } from '../src/planets.js';
+$;
+/*import { Person } from '../src/planets.js';
 
-$(document).ready(function() {
+/*$(document).ready(function() {
   $('#personform').submit(function(event) {
     event.preventDefault();
     const nameInput = $("input#person").val();
@@ -14,4 +15,4 @@ $(document).ready(function() {
   });
 
 });
-  
+  */
